10505d6d91d4e545467caf298df79071649f108b
Jason <jason@zgwit.com>
2023-04-24 14:24:57 +0800

    升级

--- config/config.go
+++ config/config.go
@@ -4,14 +4,12 @@ import (
 	"github.com/zgwit/iot-master/v3/pkg/db"
 	"github.com/zgwit/iot-master/v3/pkg/log"
 	"github.com/zgwit/iot-master/v3/pkg/mqtt"
-	"github.com/zgwit/iot-master/v3/pkg/oem"
 	"github.com/zgwit/iot-master/v3/pkg/web"
 )
 
 func Load() {
 	_ = log.Load()
 	_ = web.Load()
-	_ = oem.Load()
 	_ = db.Load()
 	_ = mqtt.Load()
 }
